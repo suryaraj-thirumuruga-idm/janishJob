034a8b8a3ba02e6cd8a2927b15ef748085ebab5d
suryaraj.technical.idm@gmail.com <123456789+suryaraj-thirumuruga-idm@users.noreply.github.com>
2025-04-14 16:46:47 +0530

    14/04/2025

--- js/script.js
+++ js/script.js
@@ -1,1 +1,44 @@
 console.log("Hello Janish,,,");
+
+// var firstNumber = 10 ;
+
+// var firstNumber =90;
+
+let number = 90 ;
+
+number =99;
+
+const a = 98;
+// a =90;
+
+// console.log(a);
+
+// alert('warning');
+
+// confirm('r u exit..');
+
+// prompt('enter your name..');
+
+// print()
+
+// close 
+
+// open('./');
+
+let nu = 10 
+
+console.log(nu.toString());
+
+let name = "11";
+
+console.log(parseInt(name));
+
+
+
+
+
+
+
+
+
+
