e03a28f2982f60f5d293ed64d380d1aef76e69fa
suryaraj.technical.idm@gmail.com <123456789+suryaraj-thirumuruga-idm@users.noreply.github.com>
2025-04-21 19:15:59 +0530

    Array

--- js/script.js
+++ js/script.js
@@ -25,13 +25,138 @@ const a = 98;
 
 // open('./');
 
-let nu = 10 
+// let nu = 10 
 
-console.log(nu.toString());
+// console.log(nu.toString());
 
-let name = "11";
+// let name = "11";
 
-console.log(parseInt(name));
+// console.log(parseInt(name));
+
+
+// let name1 = "SuRYaRaj";
+// console.log(name1);
+// console.log(name1.length);
+// console.log(name1[6]);
+// console.log(name1.indexOf('R'));
+// console.log(name1.lastIndexOf('j'));
+// console.log(name1.repeat(5));
+// console.log(name1.toUpperCase());
+
+
+// name1[8]="janish";
+
+// console.log(name1);
+
+
+// console.log(Math.E);
+// console.log(Math.PI);
+// console.log(Math.ceil(9.1));
+// console.log(Math.floor(90.9));
+// console.log(Math.max(90,80));
+// console.log(Math.min(80,70));
+// console.log(Math.pow(2,5));
+// console.log(Math.random());
+
+
+
+// +,-,*,/,%
+
+// = , 
+
+// < > <= >= != == === ;
+
+// AND &&   OR ||   NOT ! 
+
+
+// ++ -- ;
+
+//  - + ;
+
+
+// Value ? true : false ;
+
+// if(10<4){
+//     console.log("true..");    
+// }
+
+// else if(40<5){
+//     console.log("2nd value crt...");
+    
+// }
+// else if((10<40)&&(40<6)){
+//     console.log("AND Op True...");
+    
+// }
+// else if((10<5)||(5<4)){
+//     console.log("OR SINGLE VALUE CRT..");
+    
+// }
+
+// else{
+//     console.log("Error...");
+    
+// }
+
+
+
+// let arr=[1,2,3,4,5,12.5,'s',"SURYA",true];
+
+// arr[9]="janish"
+
+// arr.push("One")
+// arr.push("Two");
+
+// arr.pop()
+
+// arr.unshift("IDM")
+// arr.unshift("IDM1")
+// arr.shift()
+
+
+
+// console.log(arr);
+// console.log(arr.length);
+ 
+// arr.splice(3,0,"kia");
+// console.log(arr);
+
+
+// let MultipleArr = [
+//     [1,2,3,4,5,[1,3,4,5,6,["surya",["janish",["welcome..."]]]]]
+// ]
+
+
+// const arr =[1,2,3,4];
+
+// arr[4]="surya";
+
+// console.log(arr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
